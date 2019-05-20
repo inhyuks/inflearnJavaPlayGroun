e03ff6b2eae44bf3609e7e39f7a6f62a289b01fa
inhyuks <sih2341@naver.com>
2019-05-20 15:01:03 +0900

    변수와 입력을 이용한계산

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,9 +1,11 @@
 package gugudan;
 
+import java.util.Scanner;
+
 public class Gugudan {
 
 	public static void main(String[] arg) {
-		System.out.println(2*1);
+		/*System.out.println(2*1);
 		System.out.println(2*2);
 		System.out.println(2*3);
 		System.out.println(2*4);
@@ -21,6 +23,20 @@ public class Gugudan {
 		System.out.println(3*6);
 		System.out.println(3*7);
 		System.out.println(3*8);
-		System.out.println(3*9);
+		System.out.println(3*9);*/ // 2단3단 System..out.println 으로 출력
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("구구단을 출력할 단은? : ");
+		int i = in.nextInt();
+		System.out.println(i*1);
+		System.out.println(i*2);
+		System.out.println(i*3);
+		System.out.println(i*4);
+		System.out.println(i*5);
+		System.out.println(i*6);
+		System.out.println(i*7);
+		System.out.println(i*8);
+		System.out.println(i*9);
+		
 	}
 }
