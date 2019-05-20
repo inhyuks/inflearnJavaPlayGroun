1418ed7e8505d2ba0fbe1ea244bb14255fd8e609
inhyuks <sih2341@naver.com>
2019-05-20 15:12:56 +0900

    배열을 이용한 계산

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -46,7 +46,7 @@ public class Gugudan {
 		/*for(int i = 1; i<10; i++) {
 			System.out.println(i*5);
 		}*/ // for 반복
-		
+		/*	
 		System.out.println("구구단을 출력할 단은 : ");
 		Scanner in = new Scanner(System.in);
 		int num = in.nextInt();
@@ -57,6 +57,16 @@ public class Gugudan {
 			}
 		}
 		else System.out.println("2이상 , 9이하의 값만 받을수 있습니다.");
+		*/ // if 문을 이용한 입력값 확인
 		
+		/*int[] num = new int[9];
+		
+		for(int i=0; i<9; i++) {
+			num[i]=(i+1)*2;
+		}
+		
+		for(int i=0; i<9; i++) {
+			System.out.println(num[i]);
+		}*/ // 배열을 이용한 계산
 	}
 }
