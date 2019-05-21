49779e233be3ae28ce0e96645f4084b6e75caecf
inhyuks <sih2341@naver.com>
2019-05-21 11:30:07 +0900

    가상달력찍기

--- src/gugudan/calendar/Calendars.java
+++ src/gugudan/calendar/Calendars.java
@@ -20,21 +20,21 @@ public class Calendars {
 	
 	public static void main(String[] arg) {
 		Scanner in = new Scanner(System.in);
-	/*	System.out.println("�� �� ȭ �� �� �� ��");
+	/*	System.out.println("일 월 화 수 목 금 토");
 		System.out.println("1 2 3 4 5 6 7");
 		System.out.println("8 9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");*/
 		//print 로 달력 출력
 		
-		////숫자를 입력받아 입력받은 달의 최대 일수 구하기
 		/*System.out.println("달을 입력하세요 : ");
 		Calendars cal = new Calendars();
 		int month = in.nextInt();
 		System.out.printf("%d월은 %d일까지 있습니다.",month,cal.getMaxDayOfMonth(month));
 		cal.printSampleCalendar();*/
+		////숫자를 입력받아 입력받은 달의 최대 일수 구하기
 		
-		System.out.println("반복횟수를 입력하세요 : ");
+		/*System.out.println("반복횟수를 입력하세요 : ");
 		int cnt = in.nextInt();
 		Calendars cal = new Calendars();
 		int[] month = new int[cnt];
@@ -46,6 +46,21 @@ public class Calendars {
 		
 		for(int i=0; i<cnt; i++) {
 			System.out.printf("%d월은 %d일까지 있습니다.\n",month[i],cal.getMaxDayOfMonth(month[i]));
+		}*/ 
+		// 반복 입력 가능하게 변경
+		
+		String prompt = ">";
+		System.out.println("달을 입력하세요 : ");
+		System.out.print(prompt);
+		int month = in.nextInt();
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("------------");
+		for(int i=1; i<=MAX_DAYS[month-1]; i++) {
+			System.out.print(i+" ");
+			if(i%7==0) {
+				System.out.println();
+			}
 		}
+		
 	}
 }
