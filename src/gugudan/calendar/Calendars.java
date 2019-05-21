0dddbdf2bb722ebdbd11a057883e6509000c1a2b
inhyuks <sih2341@naver.com>
2019-05-21 11:42:31 +0900

    가상달력찍기 UPDATE 윤년체크

--- src/gugudan/calendar/Calendars.java
+++ src/gugudan/calendar/Calendars.java
@@ -4,12 +4,26 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 public class Calendars {
-	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,30,31,30,31,30};
-	
-	public int getMaxDayOfMonth(int month) {
-		return MAX_DAYS[month-1];
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
+	public int getMaxDayOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
 	public void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("1 2 3 4 5 6 7");
@@ -17,7 +31,7 @@ public class Calendars {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
-	
+
 	public static void main(String[] arg) {
 		Scanner in = new Scanner(System.in);
 	/*	System.out.println("일 월 화 수 목 금 토");
@@ -48,18 +62,28 @@ public class Calendars {
 			System.out.printf("%d월은 %d일까지 있습니다.\n",month[i],cal.getMaxDayOfMonth(month[i]));
 		}*/ 
 		// 반복 입력 가능하게 변경
-		
-		String prompt = ">";
-		System.out.println("달을 입력하세요 : ");
-		System.out.print(prompt);
-		int month = in.nextInt();
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("------------");
-		for(int i=1; i<=MAX_DAYS[month-1]; i++) {
-			System.out.print(i+" ");
-			if(i%7==0) {
-				System.out.println();
+		int year=0,month=0;
+		while(true) {
+			Calendars cal = new Calendars();
+			String prompt = ">";
+			System.out.println("연과 달을 입력하세요 : ");
+			System.out.print(prompt);
+			year = in.nextInt();
+			if(year==-1) {
+				System.out.println("종료되었습니다.");
+				break;
 			}
+			month = in.nextInt();
+			System.out.printf("    << %d년 %d월 >>\n",year,month);
+			System.out.println("일 월 화 수 목 금 토");
+			System.out.println("------------");
+			for(int i=1; i<=cal.getMaxDayOfMonth(year,month); i++) {
+				System.out.print(i+" ");
+				if(i%7==0) {
+					System.out.println();
+				}
+			}
+			System.out.println();
 		}
 		
 	}
